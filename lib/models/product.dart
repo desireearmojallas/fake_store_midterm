6c22b8b5c280992e5f0f5f153f5185176d4e915f
Desiree Armojallas <desiree.armojallas.20@usjr.edu.ph>
2022-07-13 03:42:55 +0800

    Fake Store API Final Commit

---
+++ lib/models/product.dart
@@ -0,0 +1,27 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'product.g.dart';
+
+@JsonSerializable()
+class Product {
+  int id;
+  String title;
+  double price;
+  String? description;
+  String? image;
+  String? category;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    this.description,
+    this.image,
+    this.category,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> data) =>
+      _$ProductFromJson(data);
+
+  Map<String, dynamic> toJson() => _$ProductToJson(this);
+}
